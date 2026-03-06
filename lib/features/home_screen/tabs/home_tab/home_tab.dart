0b9a59e58eacd41d5ee118f6ee86de08392cc005
space_app <mahmoudsa722@gmail.com>
2026-03-06 06:03:10 +0200

    + create custom_elevated_button widget

--- lib/features/home_screen/tabs/home_tab/home_tab.dart
+++ lib/features/home_screen/tabs/home_tab/home_tab.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:movies/core/assets/app_assets.dart';
-import 'package:movies/core/colors/app_color.dart';
-
-import '../../../../core/strings/app_string.dart';
-import '../../../../core/styles/app_style.dart';
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
 
