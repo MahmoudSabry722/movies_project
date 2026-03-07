4f9e0e8de68eae48e19cf50008b49c8970d1121b
space_app <mahmoudsa722@gmail.com>
2026-03-07 12:18:41 +0200

    +Advanced text field additions

--- lib/features/home_screen/tabs/home_tab/home_tab.dart
+++ lib/features/home_screen/tabs/home_tab/home_tab.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:movies/core/assets/app_assets.dart';
-import 'package:movies/core/strings/app_string.dart';
-import 'package:movies/core/widgets/custom_text_form_field.dart';
-
-import '../../../../core/validation/validation.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
