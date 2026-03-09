5dfe4aa3754e1fa52684a55b2985aa0df90b1eb9
ALIENWARE <abdogad963@gmail.com>
2026-03-09 08:48:56 +0200

    Fixed overflow in update_profile.dart

--- lib/features/update_profile/presentation/screens/update_profile.dart
+++ lib/features/update_profile/presentation/screens/update_profile.dart
@@ -22,6 +22,7 @@ class _UpdateTabState extends State<UpdateProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
       backgroundColor: AppColor.primaryColor,
       appBar: AppBar(
         iconTheme: IconThemeData(
@@ -31,113 +32,114 @@ class _UpdateTabState extends State<UpdateProfile> {
         title: Text(AppString.pickAvatar, style: AppStyle.font16GoldW400),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: REdgeInsets.all(16),
-        child: Column(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Padding(
-                    padding: REdgeInsets.only(top: 37),
-                    child: GestureDetector(
-                      onTap: () {
-                        showModalBottomSheet(
-                          context: context,
-                          backgroundColor: AppColor.tertiaryColor,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.vertical(
-                              top: Radius.circular(24.r),
-                            ),
+        body: SingleChildScrollView(
+          padding: REdgeInsets.only(
+            left: 16,
+            right: 16,
+            top: 16,
+            bottom: MediaQuery
+                .of(context)
+                .viewInsets
+                .bottom + 16.h,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Padding(
+                  padding: REdgeInsets.only(top: 37),
+                  child: GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet(
+                        isScrollControlled: true,
+                        context: context,
+                        backgroundColor: AppColor.tertiaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(24.r),
                           ),
-                          builder: (context) {
-                            return Padding(
-                              padding: REdgeInsets.all(20),
-                              child: GridView.count(
-                                shrinkWrap: true,
-                                crossAxisCount: 3,
-                                crossAxisSpacing: 20.w,
-                                mainAxisSpacing: 20.h,
-                                children: [
-                                  avatarItem(AppImage.avatar1),
-                                  avatarItem(AppImage.avatar2),
-                                  avatarItem(AppImage.avatar3),
-                                  avatarItem(AppImage.avatar4),
-                                  avatarItem(AppImage.avatar5),
-                                  avatarItem(AppImage.avatar6),
-                                  avatarItem(AppImage.avatar7),
-                                  avatarItem(AppImage.avatar8),
-                                  avatarItem(AppImage.avatar9),
-                                ],
-                              ),
-                            );
-                          },
-                        );
-                      },
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(12),
-                        child: Image.asset(
-                          selectedAvatar,
-                          width: 150.w,
-                          height: 150.h,
-                          fit: BoxFit.cover,
                         ),
+                        builder: (context) {
+                          return Padding(
+                            padding: REdgeInsets.all(20),
+                            child: GridView.count(
+                              shrinkWrap: true,
+                              crossAxisCount: 3,
+                              crossAxisSpacing: 20.w,
+                              mainAxisSpacing: 20.h,
+                              children: [
+                                avatarItem(AppImage.avatar1),
+                                avatarItem(AppImage.avatar2),
+                                avatarItem(AppImage.avatar3),
+                                avatarItem(AppImage.avatar4),
+                                avatarItem(AppImage.avatar5),
+                                avatarItem(AppImage.avatar6),
+                                avatarItem(AppImage.avatar7),
+                                avatarItem(AppImage.avatar8),
+                                avatarItem(AppImage.avatar9),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        selectedAvatar,
+                        width: 150.w,
+                        height: 150.h,
+                        fit: BoxFit.cover,
                       ),
                     ),
                   ),
                 ),
-                SizedBox(height: 34.h),
-                CustomTextFormField(
-                  hintText: AppString.name,
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: SvgPicture.asset(AppIcon.user),
-                  ),
+              ),
+              SizedBox(height: 34.h),
+              CustomTextFormField(
+                hintText: AppString.name,
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: SvgPicture.asset(AppIcon.user),
                 ),
-                SizedBox(height: 20.h),
-                CustomTextFormField(
-                  hintText: AppString.password,
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: SvgPicture.asset(AppIcon.phone),
-                  ),
+              ),
+              SizedBox(height: 20.h),
+              CustomTextFormField(
+                hintText: AppString.password,
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: SvgPicture.asset(AppIcon.phone),
                 ),
-                SizedBox(height: 20.h),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    AppString.resetPassword,
-                    style: AppStyle.font20WhiteW400,
-                  ),
+              ),
+              SizedBox(height: 20.h),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  AppString.resetPassword,
+                  style: AppStyle.font20WhiteW400,
                 ),
-              ],
-            ),
+              ),
+              SizedBox(height: 220.h),
+              CustomElevatedButton(
+                elevatedButtonTextStyle: AppStyle.font20WhiteW400,
+                backGroundColor: AppColor.elevatedButtonColor,
+                elevatedButtonText: AppString.deleteAccount,
+                elevatedButtonAction: () {},
+              ),
+              SizedBox(height: 20.h),
+              CustomElevatedButton(
+                elevatedButtonTextStyle: AppStyle.font20BlackW600.copyWith(
+                  fontWeight: FontWeight.w400,
+                ),
+                backGroundColor: AppColor.secondaryColor,
+                elevatedButtonText: AppString.updateData,
+                elevatedButtonAction: () {},
+              ),
+              SizedBox(height: 20.h),
 
-            Spacer(),
-
-            Column(
-              children: [
-                CustomElevatedButton(
-                  elevatedButtonTextStyle: AppStyle.font20WhiteW400,
-                  backGroundColor: AppColor.elevatedButtonColor,
-                  elevatedButtonText: AppString.deleteAccount,
-                  elevatedButtonAction: () {},
-                ),
-                SizedBox(height: 20.h),
-                CustomElevatedButton(
-                  elevatedButtonTextStyle: AppStyle.font20BlackW600.copyWith(
-                    fontWeight: FontWeight.w400,
-                  ),
-                  backGroundColor: AppColor.secondaryColor,
-                  elevatedButtonText: AppString.updateData,
-                  elevatedButtonAction: () {},
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+            ],
+          ),
+        )
     );
   }
 
